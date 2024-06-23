a8e0ab4a5d54f1e3ea69aade59207c1210fcdef3
Dmitriy209 <dim286754@gmail.com>
2024-06-23 20:07:48 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "String.Split метод возвращает строковый массив, содержащий подстроки данного экземпляра, разделенные элементами заданной строки или массива знаков Юникода.";
+            string[] arrayText = text.Split(' ');
+
+            for (int i = 0; i < arrayText.Length; i++)
+            {
+                Console.WriteLine(arrayText[i]);
+            }
+        }
+    }
+}
